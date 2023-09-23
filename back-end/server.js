8eaa05a59c916ebfc385f7725562075040d050d4
Swami Mahale <swamimahale1712@gmail.com>
2023-09-23 20:32:11 +0530

    update package.json again

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { body, validationResult } = require('express-validator');
-const userRoute = require('./routes/user')
-require('dotenv').config()
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { body, validationResult } = require("express-validator");
+const userRoute = require("./routes/user");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,9 +15,7 @@ app.use(cors());
 
 app.use("/", userRoute);
 
-
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
-
